0682261000a65bb56d230083db6b164426ad90e3
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2023-12-24 23:50:21 -0500

    rn

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from sys import path
 path.append(str(Path(__file__).parent.parent))
+path.append(str(Path(__file__).parent / "src"))
 
 from dotenv import load_dotenv
 load_dotenv()
