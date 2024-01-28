11bf2802d31275cab1e2fa65eda8e40c31448e3c
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2024-01-27 21:10:46 -0500

    Add pre-commit package and update sqlfluff configuration

---
+++ tests/test_tests.py
@@ -0,0 +1,13 @@
+from unittest import TestCase, main
+
+
+class TestTests(TestCase):
+    """Test the test suite itself."""
+
+    def test_tests(self):
+        """Test that tests are working."""
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    main()
