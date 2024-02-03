797a954fc035ef2617b26c5e8774e249681d3363
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2024-02-03 16:16:45 -0500

    rm load_dotenv reference rm temp files

--- __init__.py
+++ __init__.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 from sys import path
+
 path.append(str(Path(__file__).parent.parent))
 path.append(str(Path(__file__).parent / "src"))
-
-from dotenv import load_dotenv
-load_dotenv()
