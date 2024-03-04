e15306b5e0e74e739d9820b0eadb9f38bdaba698
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2024-03-03 22:15:47 -0500

    Deleted unnecessary SQL files and tables

---
+++ src/queries/agg/derivative1/running_assessment_score.sql
@@ -0,0 +1,37 @@
+drop table if exists agg.running_assessment_score;
+select
+    *
+    , cum_score_weight_prod / cum_weight running_score
+
+into agg.running_assessment_score
+from (
+
+    select
+        s.student_id
+        , s.course_id
+        , s.assessment_id
+        , s.assessment_type_id
+        , sum(s.score) over (
+            partition by s.course_id, s.student_id
+            order by s.date_due
+        ) cum_score
+        , sum(s.weight) over (
+            partition by s.course_id, s.student_id
+            order by s.date_due
+        ) cum_weight
+        , sum(s.score * s.weight) over (
+            partition by s.course_id, s.student_id
+            order by s.date_due
+        ) cum_score_weight_prod
+        , s.weight
+        , s.is_banked
+        , s.date_due
+    from main.v_assessment_staging s
+
+    where s.is_weighted = 1
+) s
+
+order by
+    s.course_id
+    , s.student_id
+    , s.date_due
---
+++ src/queries/first30/0/students.sql
@@ -0,0 +1,5 @@
+select
+    si.id
+into first30.students
+from main.student_info si
+where (date_unregistration is null or date_unregistration > 30)
