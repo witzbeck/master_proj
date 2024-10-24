47f39b7c5cd242f46e900512856f19e2d27d3d68
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2024-10-24 07:16:02 -0400

    render edm venn diagram

--- src/queries/00_landing/student_info.sql
+++ src/queries/00_landing/student_info.sql
@@ -6,7 +6,7 @@ select cast(id_student as int) AS student_id,
             when gender = 'F' then '1'
             when gender = 'M' then '0'
             else NULL
-        end as bit
+        end as tinyint
     ) AS is_female,
     cast(imd_band as varchar(7)) AS imd_band,
     cast(highest_education as varchar(27)) AS highest_education,
@@ -19,7 +19,7 @@ select cast(id_student as int) AS student_id,
             when disability = 'Y' then '1'
             when disability = 'N' then '0'
             else disability
-        end as bit
+        end as tinyint
     ) AS has_disability,
     cast(final_result as varchar(11)) AS final_result
 from student_info
