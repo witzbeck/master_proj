df020607e0bfd8fd9130962844982ff17f7f0585
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2024-10-26 11:27:57 -0400

    fix presentation to rm rendered cell outputs for performance

--- src/utils/get_figures.py
+++ src/utils/get_figures.py
@@ -7,20 +7,25 @@ from pathlib import Path
 from IPython.display import Image
 from matplotlib.axis import Axis
 from matplotlib.figure import Figure
-from matplotlib.pyplot import subplots
+from matplotlib.pyplot import savefig, subplots
 from matplotlib_venn import venn3
+from pandas import DataFrame
 from pymupdf import IRect, Page, Rect
+from pymupdf import open as open_pdf
+from seaborn import color_palette, displot, set_theme
 
 from utils.constants import FIGURES_PATH, LOGOS_PATH, PRESENTATION_RESEARCH_PATH
 
 THEME_STYLE = "whitegrid"
+THEME_CONTEXT = "talk"
+set_theme(style=THEME_STYLE, context=THEME_CONTEXT)
 (GENERATED_FIGURES_PATH := FIGURES_PATH / "generated").mkdir(
     parents=True, exist_ok=True
 )
 
 
 def get_page_from_file(source_file: Path, page_number: int) -> Page:
-    doc = open(str(source_file))  # open a document
+    doc = open_pdf(str(source_file))  # open a document
     page = doc[page_number - 1]
     return page
 
@@ -71,7 +76,7 @@ class FigSizes(Enum):
 
 def get_edm_venn(
     center_size: int = 8,
-    sides_size: int = 8,
+    sides_size: int = 10,
     sets_size: int = 6,
     figsize: tuple[int, int] = (10, 10),
     dpi: int = 200,
@@ -99,7 +104,28 @@ def get_edm_venn(
     venn = venn3(subsets=subsets, set_labels=None, ax=ax)
     for name, label in zip(subset_labels, venn.subset_labels, strict=True):
         label.set_text(name)
+    print("Educational Data Mining and Learning Analytics")
+    print("Relationship with other fields of research")
     return fig, ax
+
+
+def get_imd_band_displot(df: DataFrame) -> tuple[Figure, Axis]:
+    """Create a distribution plot of IMD bands."""
+    color_palette("Spectral", n_colors=4)
+    ax = displot(
+        df.sort_values("module_code"),
+        x="final_result",
+        col="imd_band",
+        row="is_female",
+        col_order=[f"{k}-{k + 10}%" for k in range(0, 100, 10)],
+        binwidth=1,
+        height=3,
+        facet_kws={"margin_titles": True},
+        stat="probability",
+        hue="final_result",
+    )
+    print("Final Result by IMD Band and is_female")
+    return ax
 
 
 @dataclass(frozen=True)
@@ -129,6 +155,7 @@ class ProjectFigure:
             ret = Image(filename=self.current_file, **self.figsize.asdict)
         elif self.func is not None:
             self.func()
+            savefig(self.filepath)
             ret = Image(filename=self.filepath, **self.figsize.asdict)
         else:
             raise ValueError("No image available")
@@ -161,19 +188,19 @@ class SharedFigures(Enum):
         figsize=FigSizes.FULL.value,
     )
     ABROCA_LOGREG_ETREE = ProjectFigure(
-        "ABROCA LogReg ETree",
+        "ABROCA LogReg ETree Curves",
         "The ABROCA statistic rendered for two models split by is_female and has_disability",
         current_file=FIGURES_PATH / "abroca_logreg_271_0_etree_358_0_.png",
         figsize=FigSizes.XL.value,
     )
     BAYESIAN_ROPE_WINDOWPANE = ProjectFigure(
-        "Bayesian ROPE Windowpane",
+        "Windowpane Plot of Bayesian Model Comparisons",
         "A windowpane plot of Bayesian model comparisons using the results of a Bayesian signed rank test using a ROPE of 0.002",
         current_file=FIGURES_PATH / "bayes_window_20_16_top_100.png",
         figsize=FigSizes.FULL.value,
     )
     FREQUENTIST_ROPE_WINDOWPANE = ProjectFigure(
-        "Frequentist ROPE Windowpane",
+        "Windowpane Plot of Frequentist Model Comparisons",
         "A windowpane plot of frequentist model comparisons using the results of the non-parametric Nemenyi test",
         current_file=FIGURES_PATH / "freq_window_20_16_top_100.png",
         figsize=FigSizes.FULL.value,
@@ -193,7 +220,7 @@ class SharedFigures(Enum):
     N_DAYS_ACTIVE_BY_FINAL_RESULT = ProjectFigure(
         "Days Active by Final Result",
         "The distribution of student active days in each course",
-        current_file=FIGURES_PATH / "n_days_active_by_final_result.png",
+        current_file=FIGURES_PATH / "n_days_active.png",
         figsize=FigSizes.FULL.value,
     )
     FINAL_RESULT_BY_STUDENT_COUNT = ProjectFigure(
@@ -232,19 +259,29 @@ class SharedFigures(Enum):
         current_file=FIGURES_PATH / "oulad_15_imd.png",
         figsize=FigSizes.FULL.value,
     )
+    BAYES_ROPE_PDF = ProjectFigure(
+        "Bayes On One Model",
+        "A probability density function resulting from a Bayesian Signed Rank Test",
+        current_file=FIGURES_PATH / "rope_rforest_519_0__etree_358_0_.png",
+    )
+    BAYES_ROPE_POSTERIOR = ProjectFigure(
+        "Bayes On Two Models",
+        "A Bayesian posterior plot resulting from a Bayesian hierarchical correlated t-test",
+        current_file=FIGURES_PATH / "rope_on_two_hxg_boost_531_0__hxg_boost_540_0_.png",
+    )
+    HXGBOOST_ABROCA_IS_FEMALE = ProjectFigure(
+        "HXGBoost ABROCA Is Female Demo",
+        "The ABROCA statistic rendered for a model split by is_female",
+        current_file=FIGURES_PATH / "hxg_boost_abroca_is_female_demo.png",
+    )
+    HXBOOST_ROC_DEMO = ProjectFigure(
+        "HXBoost ROC Demo",
+        "The ROC curve for an HXGBoost model",
+        current_file=FIGURES_PATH / "hxg_boost_roc_demo.png",
+    )
 
 
 class PaperFigures(Enum):
-    BAYES_ROPE_PDF = PaperFigure(
-        "Bayes On One",
-        "A probability density function resulting from a Bayesian Signed Rank Test",
-        current_file="rope_rforest_519_0__etree_358_0_.png",
-    )
-    BAYES_ROPE_POSTERIOR = PaperFigure(
-        "Bayes On Two",
-        "A Bayesian posterior plot resulting from a Bayesian hierarchical correlated t-test",
-        current_file="rope_on_two_hxg_boost_531_0__hxg_boost_540_0_.png",
-    )
     OULAD_COURSE_BREAKDOWN = PaperFigure(
         "OULAD Course Breakdown",
         "A breakdown of the OULAD dataset by course modules and domains",
@@ -290,21 +327,6 @@ class PaperFigures(Enum):
         "The mean and standard deviation of fit time and ROC AUC by model type",
         current_file="roc_fit_by_mtype.png",
     )
-    WINDOWPANE_FREQUENTIST = PaperFigure(
-        "Windowpane Plot of Frequentist Model Comparisons",
-        "A windowpane plot of frequentist model comparisons using the results of the non-parametric Nemenyi test",
-        current_file="freq_window_20_16_top_100.png",
-    )
-    WINDOWPANE_BAYESIAN = PaperFigure(
-        "Windowpane Plot of Bayesian Model Comparisons",
-        "A windowpane plot of Bayesian model comparisons using the results of a Bayesian signed rank test using a ROPE of 0.002",
-        current_file="bayes_window_20_16_top_100.png",
-    )
-    ABROCA_MODEL_CURVES = PaperFigure(
-        "ABROCA Model Curves",
-        "The ABROCA statistic rendered for two models split by is_female and has_disability",
-        current_file="abroca_logreg_271_0_etree_358_0_.png",
-    )
     ABROCA_REGRESSION = PaperFigure(
         "ABROCA Regression",
         "The relationship between ABROCA and demographic balance and mean test ROC AUC for the top four models",
@@ -336,12 +358,31 @@ class PresentationFigures(Enum):
         current_file=FIGURES_PATH / "auto_fe.png",
         func=partial(
             save_figure_from_page,
-            source_file=PRESENTATION_RESEARCH_PATH,
+            source_file=PRESENTATION_RESEARCH_PATH
+            / "AutoML Feature Engineering for Student Modeling Yields High Accuracy, but Limited Interpretability.pdf",
             target_path=GENERATED_FIGURES_PATH / "AutoML_Feature_Engineering.png",
             page_number=18,
             dpi=300,
             top_left=(0, 0),
             bot_right=(500, 500),
+        ),
+    )
+    CRITICAL_DIFFERENCE_NEMENYI = PresentationFigure(
+        "Critical Difference Nemenyi",
+        "Critical difference diagram based on results from post-hoc Nemenyi tests",
+        source_file=PRESENTATION_RESEARCH_PATH
+        / "Evaluating Predictive Models of Student Success Closing the Methodological Gap.pdf",
+        current_file=FIGURES_PATH / "nemenyi_critical_dif.png",
+        figsize=FigSizes.FULL.value,
+        func=partial(
+            save_figure_from_page,
+            source_file=PRESENTATION_RESEARCH_PATH
+            / "Evaluating Predictive Models of Student Success Closing the Methodological Gap.pdf",
+            target_path=GENERATED_FIGURES_PATH / "Critical_Difference_Nemenyi.png",
+            page_number=13,
+            dpi=300,
+            top_left=(25, 60),
+            bot_right=(950, 410),
         ),
     )
     SCIKIT_LEARN_LOGO = PresentationFigure(
@@ -405,28 +446,72 @@ class PresentationFigures(Enum):
         figsize=FigSizes.LOGO.value,
     )
     IMD_BAND_IRELAND = PresentationFigure(
-        "IMD Band Ireland",
-        "The distribution of IMD bands for Ireland",
+        "IMD Band Northern Ireland",
+        "The distribution of IMD bands in Northern Ireland for 2017",
         source_table="landing.student_info",
         current_file=FIGURES_PATH / "imd.png",
+        figsize=FigSizes.DEMOG.value,
     )
     AGE_BAND_BY_STUDENT = PresentationFigure(
         "Age Band by Student",
         "The distribution of age bands by student count",
         source_table="landing.student_info",
         current_file=FIGURES_PATH / "age_band_by_student.png",
+        figsize=FigSizes.DEMOG.value,
+    )
+    IMD_BAND_BY_STUDENT = PresentationFigure(
+        "IMD Band by Student",
+        "The distribution of IMD bands by student count",
+        source_table="landing.student_info",
+        current_file=FIGURES_PATH / "imd_band_by_student.png",
+        figsize=FigSizes.DEMOG.value,
+    )
+    REGION_BY_STUDENT = PresentationFigure(
+        "Region by Student",
+        "The distribution of regions by student count",
+        source_table="landing.student_info",
+        current_file=FIGURES_PATH / "region_by_student.png",
+        figsize=FigSizes.DEMOG.value,
+    )
+    HIGHEST_EDUCATION_BY_STUDENT = PresentationFigure(
+        "Highest Education by Student",
+        "The distribution of highest education by student count",
+        source_table="landing.student_info",
+        current_file=FIGURES_PATH / "highest_education_by_student.png",
+        figsize=FigSizes.DEMOG.value,
+    )
+    COURSE_DOMAIN_BY_STUDENT = PresentationFigure(
+        "Course Domain by Student",
+        "The distribution of course domains by student count",
+        source_table="landing.student_info",
+        current_file=FIGURES_PATH / "course_domain_by_student.png",
+        figsize=FigSizes.DEMOG.value,
+    )
+    FINAL_RESULT_BY_STUDENT = PresentationFigure(
+        "Final Result by Student",
+        "The distribution of final results by student count",
+        source_table="landing.student_info",
+        current_file=FIGURES_PATH / "final_result_by_student.png",
+        figsize=FigSizes.DEMOG.value,
     )
     OULAD_15_AGE = PresentationFigure(
         "OULAD 15 Age",
-        "The distribution of student ages for the OULAD dataset and the 2015 data for the CCC module",
+        "OULAD (Red & 2013-2014), vs 2015 data (Blue) - Age Distribution",
         current_file=FIGURES_PATH / "oulad_15_age.png",
+        figsize=FigSizes.DEMOG.value,
+    )
+    OULAD_15_IMD = PresentationFigure(
+        "OULAD 15 IMD",
+        "OULAD (Red & 2013-2014), vs 2015 data (Blue) - IMD Distribution",
+        current_file=FIGURES_PATH / "oulad_15_imd.png",
+        figsize=FigSizes.DEMOG.value,
     )
     OULAD_VS_15 = PresentationFigure(
         "OULAD vs 15",
         "An evaluation of the similarity between the published OULAD dataset and the 2015 data for CCC module",
         current_file=FIGURES_PATH / "oulad_vs_15.png",
     )
-    OULADS_STUDENTS_COURSES = PresentationFigure(
+    OULAD_STUDENT_COURSES = PresentationFigure(
         "OULADs Students Courses",
         "A breakdown of the OULAD dataset by course modules and domains",
         current_file=FIGURES_PATH / "oulad_students_courses.png",
@@ -435,14 +520,12 @@ class PresentationFigures(Enum):
         "Source ERD Model",
         "An Entity Relationship Diagram of the OULAD dataset",
         current_file=FIGURES_PATH / "raw_model.png",
+        figsize=FigSizes.FULL.value,
     )
-    HXGBOOST_ABROCA_IS_FEMALE = PresentationFigure(
-        "HXGBoost ABROCA Is Female Demo",
-        "The ABROCA statistic rendered for a model split by is_female",
-        current_file=FIGURES_PATH / "hxg_boost_abroca_is_female_demo.png",
-    )
-    HXBOOST_ROC_DEMO = PresentationFigure(
-        "HXBoost ROC Demo",
-        "The ROC curve for an HXGBoost model",
-        current_file=FIGURES_PATH / "hxg_boost_roc_demo.png",
+    TABLES_BY_SCHEMA = PresentationFigure(
+        "Tables by Schema",
+        "The number of tables in each schema",
+        source_table="landing.table_counts",
+        current_file=FIGURES_PATH / "tables_by_schema.png",
+        figsize=FigSizes.FULL.value,
     )
