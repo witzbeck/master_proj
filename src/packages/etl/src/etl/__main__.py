cf1c8481b2648889dc310eeff0427357115ce0c8
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2024-11-27 16:07:07 +0000

    add incrementals with logic defaults to load database components

--- src/packages/etl/src/etl/__main__.py
+++ src/packages/etl/src/etl/__main__.py
@@ -1,6 +1,7 @@
 from click import echo, group, option
 
-from .elt_config import QueriesDirectory, main
+from etl.elt_config import QueriesDirectory, transform_data
+from etl.load_dataset import load_dataset
 
 
 @group()
@@ -9,14 +10,23 @@ def cli():
     pass
 
 
-@cli.command()
+@cli.command
+@option("-f", "--force/--no-force", default=False, help="Force download and extraction")
+@option("-c", "--cleanup/--no-cleanup", default=True, help="Cleanup after extraction")
+@option("-v", "--verbose/--no-verbose", default=True, help="Verbose output")
+def load(force: bool, cleanup: bool, verbose: bool) -> None:
+    """Download and extract the dataset."""
+    load_dataset(force=force, cleanup=cleanup)
+
+
+@cli.command
 @option("--replace/--no-replace", default=True, help="Replace existing tables/views")
-def build(replace):
+def transform(replace):
     """Build the database."""
-    main(replace=replace)
+    transform_data(replace=replace)
 
 
-@cli.command("list-sources-without-targets")
+@cli.command
 def list_sources_without_targets():
     """List sources without targets."""
     qdir = QueriesDirectory()
@@ -25,7 +35,7 @@ def list_sources_without_targets():
         echo(source)
 
 
-@cli.command("list-targets-without-sources")
+@cli.command
 def list_targets_without_sources():
     """List targets without sources."""
     qdir = QueriesDirectory()
