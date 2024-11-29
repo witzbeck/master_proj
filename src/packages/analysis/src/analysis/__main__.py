7a43920dd9c7ab67fd5f300093078ec9b26421db
Alex Beckwith <57322567+witzbeck@users.noreply.github.com>
2024-11-29 13:52:26 +0000

    Enhance figure management: update GitHub Actions workflow to include specific figure names and add functions to retrieve figure name mappings in the analysis module.

--- src/packages/analysis/src/analysis/__main__.py
+++ src/packages/analysis/src/analysis/__main__.py
@@ -1,24 +1,7 @@
 from click import group, option
 from tqdm import tqdm
 
-from analysis.get_figures import (
-    PaperFigures,
-    PresentationFigures,
-    ProjectFigure,
-    SharedFigures,
-)
-
-
-def get_figure_name_map(
-    paper: bool = True, presentation: bool = True
-) -> dict[str, ProjectFigure]:
-    """Get a map of figure names to figure objects."""
-    figmap = {x.value.name: x.value for x in SharedFigures}
-    if paper:
-        figmap.update({x.value.name: x.value for x in PaperFigures})
-    if presentation:
-        figmap.update({x.value.name: x.value for x in PresentationFigures})
-    return figmap
+from analysis import get_figure_name_map
 
 
 @group
